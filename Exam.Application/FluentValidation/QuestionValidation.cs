62810078329ea9eeb5dfc34570d7c1378845837c
Rufane Aliyeva <rufanefa@code.edu.az>
2025-05-03 22:08:42 +0400

    fluentvalidation template added

---
+++ Exam.Application/FluentValidation/QuestionValidation.cs
@@ -0,0 +1,8 @@
+﻿using Exam.Domain.Entites;
+using FluentValidation;
+namespace Exam.Application.FluentValidation
+{
+    public class QuestionValidation : AbstractValidator<Question>
+    {
+    }
+}
---
+++ Exam.Application/FluentValidation/StudentexamValidation.cs
@@ -0,0 +1,8 @@
+﻿using Exam.Domain.Entites;
+using FluentValidation;
+namespace Exam.Application.FluentValidation
+{
+    public class StudentexamValidation : AbstractValidator<StudentExam>
+    {
+    }
+}
